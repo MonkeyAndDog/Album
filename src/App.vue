3ac72704fc0d7ca28cbc2b906f584d81d8f53526
Monkey <1466947023@qq.com>
2018-08-19 18:40:55 +0800

    实现上传功能。

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header>
+        <NavMenu></NavMenu>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Home from "./pages/Home";
+  import NavMenu from "./components/NavMenu";
+
+  export default {
+    name: 'App',
+    components: {NavMenu, Home},
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,43 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
-import router from './router'
+import router from './router/index'
+import 'jquery/dist/jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
+import VueResource from 'vue-resource'
+import * as qiniu from 'qiniu-js'
 
-Vue.config.productionTip = false
+Vue.use(VueResource);
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
+Vue.http.options.emulateJSON = true;
+Vue.http.options.crossOrigin = true;
 
-/* eslint-disable no-new */
-new Vue({
+var logged = false;
+
+router.beforeEach(function (to, from, next) {
+  if (!logged && to.path == '/admin/edit') {
+    next('/admin');
+    console.log('main' + logged);
+  } else {
+    console.log('main' + logged);
+    next();
+  }
+});
+
+export default {
+  changeLogged:function () {
+    logged = true;
+  }
+}
+
+var vm = new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  router: router,
+  components: {App},
+  template: '<App/>',
+});
+
+
